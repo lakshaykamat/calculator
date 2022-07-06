d5d3f2a7f8482e62cdd8431286e1d7d1f6887795
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-06 13:30:06 +0530

    simple js added

---
+++ docs/app.js
@@ -0,0 +1,28 @@
+//DOM elements
+const currentOpBtn = document.getElementById("current-operation")
+const previousOpBtn = document.getElementById("previous-operation")
+//Array.form is for converting HTML elements into the array
+const button = Array.from(document.getElementsByClassName("btn"))
+console.log(button);
+button.map(function (button) {
+  button.addEventListener("click", function (e) {
+    switch (e.target.textContent) {
+      case "AC":
+        currentOpBtn.textContent = ""
+        break
+      case "DEL":
+        currentOpBtn.textContent = currentOpBtn.textContent.slice(0, -1);
+        break
+        case '=':
+            try{
+                currentOpBtn.textContent = eval(currentOpBtn.textContent)
+            }
+            catch{
+                currentOpBtn.textContent = 'Invalid'
+            }
+            break
+      default:
+        currentOpBtn.textContent += e.target.textContent;
+    }
+  });
+});
